b4513c937ed05b6679d9daab7ab3fbab36257e47
Joseph <ajogujoseph0317@gmail.com>
2025-10-13 06:39:38 +0100

    feta: added the url for email confirmation andfixed the Header Avatar getIntitals bug

--- src/queries/auth.queries.ts
+++ src/queries/auth.queries.ts
@@ -11,9 +11,18 @@ export const useLogin = () => {
 };
 
 export const useRegister = () => {
+  const SITE_URL =
+    process.env.NODE_ENV === "production"
+      ? "https://dashboard-ag-vmm.vercel.app/"
+      : "http://localhost:3000";
   return useMutation({
     mutationFn: async (data) => {
-      const res = await supabase.auth.signUp(data);
+      const res = await supabase.auth.signUp({
+        data,
+        options: {
+          emailRedirectTo: `${SITE_URL}/auth/callback`,
+        },
+      });
       return res.data;
     },
   });
